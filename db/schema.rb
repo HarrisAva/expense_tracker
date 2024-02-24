159719965db03b8fe19891ce1561c17f04473752
HarrisAva <avaharris1441@gmail.com>
2024-02-24 03:29:29 -0600

    remove category from expense table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_14_120622) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_23_202514) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -24,7 +24,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_14_120622) do
     t.text "title"
     t.text "description"
     t.float "amount"
-    t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_expenses_on_category_id"
